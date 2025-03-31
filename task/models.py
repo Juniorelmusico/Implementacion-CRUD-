2e6e39b36595a2a510dfcdffa2d821e77b374291
Juniorelmusico <juniorationlj@gmail.com>
2025-03-31 01:38:34 -0500

    Implement task management API with serializers, views, and URL routing

--- task/models.py
+++ task/models.py
@@ -5,3 +5,7 @@ class tasks (models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     done = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
